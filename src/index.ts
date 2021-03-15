a3a2c477e70ba0047c8dc278bead4adc7ec3eb6a
vianarafael <vianarafaelds@gmail.com>
2021-03-15 09:36:44 +0900

    added & invoked cors library

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import './config/env'
-import "reflect-metadata";
-import {createConnection} from "typeorm";
-import * as express from "express";
-import * as bodyParser from "body-parser";
+import 'reflect-metadata';
+import {createConnection} from 'typeorm';
+import * as express from 'express';
+import * as cors from 'cors';
 import routes  from './routes'
 
 createConnection();
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
